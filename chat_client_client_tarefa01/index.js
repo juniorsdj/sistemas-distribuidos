af2029705a42f652f7b185d58f3b2381e027359f
Junior <jddsjr98@gmail.com>
2019-09-06 20:50:25 -0300

    first commit

---
+++ chat_client_client_tarefa01/index.js
@@ -0,0 +1,48 @@
+
+const my_port = process.argv[2]
+const my_address = process.argv[3]
+const destiny_port = process.argv[4]
+const destiny_address = process.argv[5]
+
+
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
+
+
+
+server.on('error', (err) => {
+    console.log(`server error:\n${err.stack}`);
+    server.close();
+});
+
+server.on('message', (msg, rinfo) => {
+    console.log(`mensagem: ${msg} de ${rinfo.address}:${rinfo.port}`);
+});
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.bind({
+    address: my_address,
+    port: my_port,
+});
+
+const sendMensage = (msgn) => {
+    const message = Buffer.from(msgn);
+    console.log(`you sent: [${message}]`)
+    server.send(message, destiny_port, destiny_address, (err) => {
+    });
+}   
+
+
+process.openStdin().addListener("data", function (d) {
+    if(d.toString().trim() == 'exit'){
+        return server.close()
+    }
+    // note:  d is an object, and when converted to a string it will
+    // end with a linefeed.  so we (rather crudely) account for that  
+    // with toString() and then trim() 
+    sendMensage(d.toString().trim())
+});
---
+++ chat_client_client_tarefa01/readme.md
@@ -0,0 +1,9 @@
+Para usar a aplicação, usar o node index.js param1 param2 param3 param4
+
+param1 = my_port    minha porta
+param2 = my_address meu endereco para meu parceiro conectar
+param3 = destiny_port   porta do meu parceiro
+param4 = destiny_address endereco do meu parceiro
+
+
+apos isso, so escrever a mensagem e dar o ok
